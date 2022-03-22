085ba44af5e04c7deac81a5e2c066867d2c245ba
Poloeli303 <91678247+Poloeli303@users.noreply.github.com>
2022-03-22 10:01:00 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ screen.fill((0,0,0))
 clock = pygame.time.Clock() #set up clock
 gameover = False #variable to run our game loop
 
+Mark = pygame.image.load('dude.png') #load your spritesheet
+Mark.set_colorkey((255, 0, 255)) #this makes bright pink (255, 0, 255) transparent (sort of)
 red = (255,0,0)
 purple = (200,0,200)
+Mark = pygame.transform.scale(Mark, (2, 2))
 
 #CONSTANTS
 LEFT=0
@@ -24,7 +27,7 @@ class Rectangular:
     def draw(self):
         pygame.draw.rect(screen, (red),(self.xpos, self.ypos, 100, 20))
     def collide(self, x, y):
-        if x>self.xpos and x<self.xpos+100 and y+40> self.ypos and y+40 < self.ypos + 20:
+        if x>self.xpos and x<self.xpos+100 and y+40> self.ypos and y +20 < self.ypos + 20:
             return self.ypos
         else:
             return False
@@ -51,6 +54,13 @@ isOnGround = False #this variable stops gravity from pulling you down more when 
 jump = pygame.mixer.Sound('jump.wav')
 music = pygame.mixer.music.load('music.wav')
 pygame.mixer.music.play(-1)
+
+#animation variables variables
+frameWidth = 16
+frameHeight = 16
+RowNum = 0 #for left animation, this will need to change for other animations
+frameNum = 0
+ticker = 0
 
 p1 = Rectangular(400, 600)
 p2 = Rectangular(100, 750)
@@ -108,7 +118,23 @@ while not gameover: #GAME LOOP##################################################
         direction = UP
         pygame.mixer.Sound.play(jump)
     
-
+    #ANIMATION-------------------------------------------------------------------
+        
+    # Update Animation Information
+    # Only animate when in motion
+    if vx>0:
+        RowNum = 0
+    if vx < 0: #left animation
+        RowNum = 1
+        # Ticker is a spedometer. We don't want Link animating as fast as the
+        # processor can process! Update Animation Frame each time ticker goes over
+        ticker+=1
+        if ticker%10==0: #only change frames every 10 ticks
+          frameNum+=1
+           #If we are over the number of frames in our sprite, reset to 0.
+           #In this particular case, there are 10 frames (0 through 9)
+        if frameNum>3: 
+           frameNum = 0
     
     #COLLISION
         
@@ -116,37 +142,37 @@ while not gameover: #GAME LOOP##################################################
     if p1.collide(Px, Py) != False:
         isOnGround = True
         vy = 0
-        Py = p1.collide(Px, Py) - 40 
+        Py = p1.collide(Px, Py) - 20 
 
     elif p2.collide(Px, Py) != False:
         isOnGround = True
         vy = 0
-        Py = p2.collide(Px, Py)  - 40
+        Py = p2.collide(Px, Py)  - 20
      
     elif p3.collide(Px, Py) != False:
         isOnGround = True
         vy = 0
-        Py = p3.collide(Px, Py)  - 40
+        Py = p3.collide(Px, Py)  - 20
         
     elif p4.collide(Px, Py) != False:
         isOnGround = True
         vy = 0
-        Py = p4.collide(Px, Py)  - 40
+        Py = p4.collide(Px, Py)  - 20
         
     elif p5.collide(Px, Py) != False:
         isOnGround = True
         vy = 0
-        Py = p5.collide(Px, Py)  - 40
+        Py = p5.collide(Px, Py)  - 20
     elif s1.collide(Px, Py) != False:
         isOnGround = True
         vy = 0
-        Py = s1.collide(Px, Py)  -40
+        Py = s1.collide(Px, Py)  -20
     print(isOnGround)
     #stop falling if on bottom of game screen
-    if Py > 760:
+    if Py > 780:
         isOnGround = True
         vy = 0
-        Py = 760
+        Py = 780
     
     #gravity
     if isOnGround == False:
@@ -161,8 +187,8 @@ while not gameover: #GAME LOOP##################################################
     # RENDER Section--------------------------------------------------------------------------------
             
     screen.fill((0,0,0)) #wipe screen so it doesn't smear
-  
-    pygame.draw.rect(screen, (purple), (Px, Py, 20, 40))
+    screen.blit(Mark, (Px,Py), (frameWidth*frameNum*2, RowNum*frameHeight*2, frameWidth, frameHeight))
+    #pygame.draw.rect(screen, (purple), (Px, Py, 20, 40))
     
     
     #class platforms
